85d6070e3daa5378527193dc7774b185ed6cff2f
Hector Muñoz <munozmoraleshector@gmail.com>
2021-08-27 10:42:31 -0500

    Ejercicios 1ra Semana

--- semana1/challenge2.js
+++ semana1/challenge2.js
@@ -1,7 +1,20 @@
+//Terminado
+
 const reverseInt = (number) => {
-    //Implementación
-}
+  //Implementación
+  let newNum = 0;
+
+  if (typeof number !== "number") {
+    throw Error("Error: Tipo de dato no admitido.");
+  }
+
+  newNum =
+    parseFloat(number.toString().split("").reverse().join("")) *
+    Math.sign(number);
+
+  return newNum;
+};
 
 module.exports = {
-    reverseInt
-}
+  reverseInt,
+};
